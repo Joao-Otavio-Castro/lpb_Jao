06c462f83900bd3741b1bb30a64caa0b5056d218
João Otávio Oliveira Castro <73490417+Joao-Otavio-Castro@users.noreply.github.com>
2021-05-04 19:39:54 +0000

    Add files via upload

---
+++ Alphacar2021-JoaoOtavioCastro/src/alphacar/entidades/Veiculo.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package alphacar.entidades;
+
+public class Veiculo {
+
+    private String marca, modelo, placa, observacoes;
+    private int ano, status;//status = 0= À venda ,1= Vendido, 3=  Anúncio cancelado
+    private float preco;
+
+    public void registrar() { //metodo de registro
+
+    }
+
+    public void consultarVendidos() { //metodo de consulta de vendas
+
+    }
+
+    public void modificarStatus(int novoStatus) { //metodo de modificar status
+
+    }
+}
